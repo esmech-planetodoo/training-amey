fa93eddd0f8230e8e4de840e0832420d1c4dfa6b
Ameya Vaidya <avaidya@esmech.com>
2020-12-14 13:25:27 +0530

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+        "name" : "College Management",
+        "version" : "1.1",
+        "depends" : ['base','sale','crm'],
+        "author" : "Esmech",
+        "description": """
+        managing college daily activities
+        """,
+        "website" : "www.esmech.com",
+        'images': [],
+        "category" : "college management",
+        'summary': 'Base Module For All E-Commerce Modules-v13',
+        "demo" : [],
+        "data" : [
+             'security/ir.model.access.csv',
+             'views/student.xml',
+         ],
+        'auto_install': False,
+        "installable": True,
+}
